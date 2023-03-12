56397078d7c57a721030cf73782885cbe9999ed2
BasileCuneo <basile.cuneo@heig-vd.ch>
2023-03-12 21:21:38 +0100

    edited script according to our directory topology

--- lab01/csf_rendu.sh
+++ lab01/csf_rendu.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-PROJECT_DIR=code
+PROJECT_DIR_BASILE=basile
+PROJECT_DIR_JEREMY=jeremy
+PROJECT_DIR_KRISTINA=kristina
 REPORT_FILE=rapport.pdf
 ARCHIVE=rendu.tar.gz
 
-if [ ! -d "$PROJECT_DIR" ]
+if [ ! -d "$PROJECT_DIR_BASILE" ]
 then
-    echo "Could not find $PROJECT_DIR directory in $(pwd)" >&2
+    echo "Could not find $PROJECT_DIR_BASILE directory in $(pwd)" >&2
     exit 1
 fi
 
+if [ ! -d "$PROJECT_DIR_JEREMY" ]
+then
+    echo "Could not find $PROJECT_DIR_JEREMY directory in $(pwd)" >&2
+    exit 1
+fi
+
+if [ ! -d "$PROJECT_DIR_KRISTINA" ]
+then
+    echo "Could not find $PROJECT_DIR_KRISTINA directory in $(pwd)" >&2
+    exit 1
+fi
 
 if [ ! -f "$REPORT_FILE" ]
 then
@@ -18,4 +31,4 @@ then
 fi
 
 echo "The following files are archived in $ARCHIVE : "
-tar --exclude='rendu.tar.gz' --exclude='*.o' --exclude='comp' --exclude='sim' --exclude='synth' -czvf $ARCHIVE $PROJECT_DIR $REPORT_FILE
+tar --exclude='rendu.tar.gz' --exclude='*.o' --exclude='comp' --exclude='sim' --exclude='synth' -czvf $ARCHIVE $PROJECT_DIR_BASILE $PROJECT_DIR_JEREMY $PROJECT_DIR_KRISTINA $REPORT_FILE
